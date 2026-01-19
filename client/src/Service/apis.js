75dd91b5d1704c6268f4385beba2d2deb31ca409
Sagar <sagar20000sharma@gmail.com>
2026-01-20 00:01:02 +0530

    add api

--- client/src/Service/apis.js
+++ client/src/Service/apis.js
@@ -1,5 +1,7 @@
- const BASE_URL = process.env.REACT_APP_BASE_URL
+ //const BASE_URL = process.env.REACT_APP_BASE_URL
 //const BASE_URL = "http://localhost:4000/api/v1"; // or whatever your backend port is
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 
 // AUTH ENDPOINTS
 export const endpoints = {
